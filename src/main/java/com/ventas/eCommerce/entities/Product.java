a2453802bd3414a00f743a352629c05acd5ea5fc
Chris-PC <christian.antognazza@gmail.com>
2023-08-18 22:01:55 -0300

    Agrego entidades al proyecto

---
+++ src/main/java/com/ventas/eCommerce/entities/Product.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ventas.eCommerce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ *
+ * @author chris
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class Product {
+
+   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToOne
+    private Image image;
+    private String brand;
+    private Double price;
+    private String category;
+    private Boolean stock;
+
+}
---
+++ src/main/java/com/ventas/eCommerce/entities/Transaction.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ventas.eCommerce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ *
+ * @author chris
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class Transaction {
+
+   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+}
