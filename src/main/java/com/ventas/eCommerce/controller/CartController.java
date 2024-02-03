3b22783d9977f766190ae352783b3b4a768ebf67
Chris-PC <christian.antognazza@gmail.com>
2024-02-03 17:14:04 -0300

    Se agrega condición de loggeo para poder agregar al carrito

--- src/main/java/com/ventas/eCommerce/controller/CartController.java
+++ src/main/java/com/ventas/eCommerce/controller/CartController.java
@@ -10,6 +10,7 @@ import com.ventas.eCommerce.entities.Product;
 import com.ventas.eCommerce.entities.User;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +33,7 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping("/addProduct/{id}")
     public String AddToCart(@PathVariable Integer id, HttpSession session) {
         User logueado=(User) session.getAttribute("usuariosession");
